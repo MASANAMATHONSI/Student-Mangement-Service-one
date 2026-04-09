dffa177fd807e45a92656cdf1d129a1803996279
MASANAMATHONSI <masanamathonsi050209@gmail.com>
2026-04-09 13:24:06 +0200

    The database and the database connection

---
+++ Dathandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Student_Mangement_Service_one
+{
+    internal class Dathandler
+    {
+        string conn = ("Server=.;Initial Catalog=BCDatabase1;User ID=sa;Password=sa2025@1");
+    }
+}
