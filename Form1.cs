45eb4ccc0bbef51e5176b2864d1ad78dc540f829
MASANAMATHONSI <masanamathonsi050209@gmail.com>
2026-04-09 12:35:53 +0200

    The form stucture

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,10 @@ namespace Student_Mangement_Service_one
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
